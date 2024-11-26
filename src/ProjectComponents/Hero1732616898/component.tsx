eba6f9a191e5e1422f2c71514551dceb8f2111e9
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-11-26 15:38:24 +0100

    feat: Add image to Hero1732616898

--- src/ProjectComponents/Hero1732616898/component.tsx
+++ src/ProjectComponents/Hero1732616898/component.tsx
@@ -3,7 +3,12 @@ import React from 'react';
 const Hero: React.FC = () => {
   
   return (
-    <div className="bg-black py-16 text-white w-full h-full">
+    <div className="bg-black py-16 text-white w-full h-full relative">
+      <div 
+        className="absolute inset-0 bg-cover bg-center bg-no-repeat filter blur-sm"
+        style={{backgroundImage: `url('https://raw.githubusercontent.com/56b81caaa87941618cfed6dfb4d34047/Blockchain_strategy_game_1732616895/main/src/assets/images/c721e239b1c349c5a91cd392a2b9cc59.jpeg')`}}
+      ></div>
+      <div className="relative container mx-auto px-4 flex flex-col md:flex-row items-center h-full">
       <div className="container mx-auto px-4 flex flex-col md:flex-row items-center h-full">
         <div className="md:w-1/2 mb-8 md:mb-0">
           <h1 className="text-4xl font-bold mb-4">Conquer the Crypto Realm</h1>
